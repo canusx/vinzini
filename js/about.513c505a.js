55b78388edd88617f037740fdb435de064f274f8
canus <canus@pop-os.localdomain>
2022-07-20 21:59:02 +0300

    init

---
+++ js/about.513c505a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvinzini_designer"]=self["webpackChunkvinzini_designer"]||[]).push([[443],{1110:function(n,i,e){e.r(i),e.d(i,{default:function(){return f}});var r=e(3396);const s={class:"about"},u=(0,r._)("h1",null,"This is an about page",-1),t=[u];function a(n,i){return(0,r.wg)(),(0,r.iD)("div",s,t)}var c=e(89);const o={},d=(0,c.Z)(o,[["render",a]]);var f=d}}]);
+//# sourceMappingURL=about.513c505a.js.map
